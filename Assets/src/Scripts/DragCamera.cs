f3a001400ae1b9773208a411bc0eee9acf0e548f
will3 <will3.git@gmail.com>
2017-01-10 21:16:19 +1300

    Basic

---
+++ Assets/src/Scripts/DragCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragCamera : MonoBehaviour {
+	public Vector3 target = new Vector3();
+	public Quaternion quaternion = Quaternion.Euler(-45, 45, 0);
+
+	public float distance = 100;
+
+	private Camera _camera;
+
+	// Use this for initialization
+	void Start () {
+		_camera = GetComponent<Camera> ();
+		if (_camera == null) {
+			Debug.LogError ("'DragCamera' must be attached to 'Camera'");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var position = quaternion * new Vector3 (0, 0, 1) * distance + target;
+
+		_camera.transform.position = position;
+		_camera.transform.LookAt (target);
+	}
+}
---
+++ Assets/src/Scripts/Planet.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cubiquity;
+
+public class Planet : MonoBehaviour {
+	private ColoredCubesVolume coloredCubesVolume;
+
+	public int width = 16;
+	public int height = 16;
+	public int depth = 16;
+
+	// Use this for initialization
+	void Start () {
+		coloredCubesVolume = gameObject.GetComponent<ColoredCubesVolume> ();
+		if (coloredCubesVolume == null) {
+			Debug.LogError ("Component 'Planet' requires 'ColoredCubesVolume'");
+		}
+
+		ColoredCubesVolumeData data = VolumeData.CreateEmptyVolumeData<ColoredCubesVolumeData>(new Region(0, 0, 0, width-1, height-1, depth-1));
+
+		coloredCubesVolume.data = data;
+
+		var color = new QuantizedColor (143, 216, 70, 255);
+
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				for (int k = 0; k < depth; k++) {
+					data.SetVoxel (i, j, k, color);
+				}
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
