470d4be237b8d3708afea956c67c2f0e6d027b1c
will3 <will3.git@gmail.com>
2017-01-21 21:38:18 +1300

    Make game a script

--- Assets/src/Scripts/Game.cs
+++ Assets/src/Scripts/Game.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using UnityEngine;
 
 namespace AssemblyCSharp
 {
-	public class Game
+	public class Game : MonoBehaviour
 	{
 		public Planet Planet;
 		public Terrian Terrian;
@@ -10,14 +11,31 @@ namespace AssemblyCSharp
 		private static Game _instance;
 		public static Game Instance {
 			get {
-				if (_instance == null) {
-					_instance = new Game ();
-				}
 				return _instance;
 			}
 		}
 
-		private Game () { }
+		void Awake() {
+			_instance = this;
+		}
+
+		private DragCamera dragCamera;
+
+		void Start() {
+			dragCamera = Camera.main.GetComponent<DragCamera> ();
+			Debug.Assert (dragCamera);
+		}
+
+		void Update() {
+			var zoomRate = 1.1f;
+			if (Input.GetKeyDown (KeyCode.Equals)) {
+				dragCamera.distance /= zoomRate; 
+			}
+
+			if (Input.GetKeyDown (KeyCode.Minus)) {
+				dragCamera.distance *= zoomRate;
+			}
+		}
 	}
 }
 
